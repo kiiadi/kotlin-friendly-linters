9e028291b410d41dd90376380cf2e12ce4ecd5ef
Kyle Thomson <kylthoms@amazon.com>
2020-06-26 12:39:16 -0700

    Fixing problem with NPE caused by Lambda local variables being picked up by the tokenizer

--- checkstyle/tst-resources/com/kiiadi/ktfriendly/checkstyle/HasKotlinHardKeyWords.java
+++ checkstyle/tst-resources/com/kiiadi/ktfriendly/checkstyle/HasKotlinHardKeyWords.java
@@ -1,8 +1,19 @@
 package com.kiiadi.ktfriendly.checkstyle;
 
+import java.util.Optional;
+import java.util.stream.Stream;
+
 public class HasInvalidIdentifiers {
     public static final String when = "blah";
     public static String in() {
         return "hello";
+    }
+    public static void betterHandleLambdas() {
+        Stream.of("hello").forEach(str -> {
+            Optional.of(str).ifPresent(value -> {
+                String strValue = value;
+                System.out.println(strValue);
+            });
+        });
     }
 }
